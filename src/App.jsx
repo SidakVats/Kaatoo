07489b40c7a6ac97b083c3411a0dc46248af6016
SidakVats <sidakvats6499@gmail.com>
2024-05-20 18:14:04 +0530

    "updated routes "

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 
 // imported react router dom
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 
 // Components
 import Navbar from "./components/Navbar/Navbar";
@@ -15,14 +15,14 @@ import Contact from "./Pages/Contact";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      
     </>
   );
 }
