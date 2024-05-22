781bf46baa219a73e8973e9736cb3d0b1e0e9343
SidakVats <sidakvats6499@gmail.com>
2024-05-22 12:17:55 +0530

    "updated circles"

--- src/components/Trusted By/TrustedBy.jsx
+++ src/components/Trusted By/TrustedBy.jsx
@@ -1,32 +1,48 @@
 import React from "react";
 import "./TrustedBy.css"; // Import your custom CSS file
 
+import image from "../../images/arrow1.svg";
+import image2 from "../../images/arrow2.svg";
+
 const TrustedBy = () => {
   return (
     <div className="container my-5">
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-3 d-flex justify-content-center">
-          <div className="circle orange">
-            <span><span className="circle-text">5+</span> years</span>
+      <div className="row justify-content-between align-items-center">
+        <div className="col-lg-3 col-md-6 text-center mb-4">
+          <p className="fs-4 text-start" style={{ textDecoration: "underline" }}>Average partnership</p>
+          <img src={image} alt="arrow" className="img-fluid arrow-img me-5 pe-5" />
+          <div className="circle orange mx-auto">
+            <span className="fs-4">
+              <span className="circle-text-large">5+</span> years
+            </span>
           </div>
         </div>
-        <div className="col-12 col-md-3 d-flex justify-content-center">
-          <div className="circle dark">
-            <span><span className="circle-text">20+</span> success stories</span>
+        <div className="col-lg-3 col-md-6 text-center mb-4">
+          <p className="fs-4 text-end" style={{ textDecoration: "underline" }}>Portfolio With</p>
+          <img src={image2} alt="arrow" className="img-fluid arrow-img ms-5 ps-5" />
+          <div className="circle dark mx-auto">
+            <div className="circle-text-container">
+              <span className="circle-text-large">20+</span>
+              <span className="circle-text">success stories</span>
+            </div>
           </div>
         </div>
-        <div className="col-12 col-md-3 d-flex justify-content-center">
-          <div className="circle light">
+        <div className="col-lg-3 col-md-6 text-center mb-4">
+          <p className="fs-4 text-start" style={{ textDecoration: "underline" }}>Focus On</p>
+          <img src={image} alt="arrow" className="img-fluid arrow-img me-5 pe-5" />
+          <div className="circle light mx-auto">
             <div className="circle-text">digitalization</div>
             <div className="circle-text">reengineering</div>
           </div>
         </div>
-        <div className="col-12 col-md-3 d-flex justify-content-center">
-          <div className="circle blue">
-            <div className="circle-text">Germany </div>
-            <div className="circle-text"> Switzerland </div>
-            <div className="circle-text">US </div>
-            <div className="circle-text"> Israel</div>
+        <div className="col-lg-3 col-md-6 text-center mb-4">
+          <p className="fs-4 text-end" style={{ textDecoration: "underline" }}>Client Geography</p>
+          <img src={image2} alt="arrow" className="img-fluid arrow-img ms-5 ps-5" />
+          <div className="circle blue mx-auto">
+            <div className="circle-text">Germany</div>
+            <div className="circle-text">Switzerland</div>
+            <div className="circle-text">US</div>
+            <div className="circle-text">Israel</div>
           </div>
         </div>
       </div>
