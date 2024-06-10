2e3d1964b5c979c82af2630c7df918664a96ff19
SidakVats <sidakvats6499@gmail.com>
2024-06-10 12:03:07 +0530

    "updated contact banner"

--- src/components/Contact Banner/Contact.jsx
+++ src/components/Contact Banner/Contact.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import image from "../../images/underline2.svg";
+
 
 
 const Contact = () => {
   return (
     <>
       <div className="container  mt-5 pt-5 mb-5">
+
+      <div className="fs-1 fw-medium">Where to find us?</div>
+        <div className="mb-4">
+          <img src={image} alt="" className="img-fluid" />
+        </div>
+
       <div class="map-responsive ">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2374.274219181808!2d-113.48427000000001!3d53.481429999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a018ccaf8c0b5d%3A0x7da0e2db6808f46a!2s9817%2044%20Ave%20NW%20%233%2C%20Edmonton%2C%20AB%20T6E%205E3%2C%20Canada!5e0!3m2!1sen!2sin!4v1717587517338!5m2!1sen!2sin" width="1200" height="600" style={{border:0}}allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className="rounded-4"></iframe>
       </div>
